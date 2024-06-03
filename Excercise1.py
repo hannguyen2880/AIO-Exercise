9eed06d86aee2233b9ce385165f43af062c16f23
hannguyen2880 <nhgh250305@gmail.com>
2024-06-03 12:04:59 +0700

    Complete 2 questions

---
+++ Excercise1.py
@@ -0,0 +1,33 @@
+def isInteger(n):
+    return n == int(n)
+
+# MAIN PROGRAM
+tp = float(input("Enter tp: "))
+if isInteger(tp) == 0:
+    print("tp must be int")
+    exit()
+
+fp = float(input("Enter fp: "))
+if isInteger(fp) == 0:
+    print("fp must be int")
+    exit()
+
+fn = float(input("Enter fn: "))
+if isInteger(fn) == 0:
+    print("fn must be int")
+    exit()
+
+if (tp <= 0) or (fp <= 0) or (fn <= 0):
+    print("tp and fp and fn must be greater than 0")
+    exit()
+
+#calculate
+precision = tp / (tp + fp)
+recall = tp / (tp + fn)
+f1_score = 2 * (precision * recall) / (precision + recall)
+
+#print result
+print("Precision is", precision)
+print("Recall is", recall)
+print("F1-Score is", f1_score)
+
